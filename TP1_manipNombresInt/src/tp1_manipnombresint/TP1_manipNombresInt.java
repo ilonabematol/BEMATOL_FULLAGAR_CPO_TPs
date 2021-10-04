7573738b15b75d18e847856f932e0db420530c86
ilonabematol <ilonabematol@localhost>
2021-10-04 15:30:55 +0200

    début du tp1 exo 1 fin

--- TP1_manipNombresInt/src/tp1_manipnombresint/TP1_manipNombresInt.java
+++ TP1_manipNombresInt/src/tp1_manipnombresint/TP1_manipNombresInt.java
@@ -26,21 +26,24 @@ public class TP1_manipNombresInt {
         int produit = 0;
         int quotien = 0;
         int diveucl =0;
-        
-        Scanner sc = new Scanner( System.in);
+        int reste=0;
+        Scanner sc = new Scanner(System.in);
         System.out.println( " please enter the the first number:");
         var1= sc.nextInt();
         System.out.println( " please enter the the first number:");
         var2= sc.nextInt();
         somme= var1+var2;
-        System.out.println(" le resultat de la somme est " + somme);
+        System.out.println(" le résultat de la somme est " + somme);
         diff = var1 - var2;
-        System.out.println(" le resultat de la différence est " + diff);
+        System.out.println(" le résultat de la différence est " + diff);
         produit = var1*var2;
-        System.out.println(" le resultat du produit est " + produit);
+        System.out.println(" le résultat du produit est " + produit);
         quotien = var1/var2;
-        System.out.println(" le resultat du quotient est " +quotien);
-        diveucl = 
+        System.out.println(" le résultat du quotient est " +quotien);
+        reste= var1%var2;
+        System.out.println(" le reste du quotient est " + reste);
+        
+        
         
     }
     
