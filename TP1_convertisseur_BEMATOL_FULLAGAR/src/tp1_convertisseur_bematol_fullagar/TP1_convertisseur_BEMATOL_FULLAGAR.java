e08792bb4bd45ab07b01125ae64af21279204ce0
ilonabematol <ilonabematol@localhost>
2021-10-04 17:13:53 +0200

    début du tp1 exo 2 suite fin

--- TP1_convertisseur_BEMATOL_FULLAGAR/src/tp1_convertisseur_bematol_fullagar/TP1_convertisseur_BEMATOL_FULLAGAR.java
+++ TP1_convertisseur_BEMATOL_FULLAGAR/src/tp1_convertisseur_bematol_fullagar/TP1_convertisseur_BEMATOL_FULLAGAR.java
@@ -28,14 +28,15 @@ public class TP1_convertisseur_BEMATOL_FULLAGAR {
         //tempK = CelciusVersKelvin(tempC);
         //System.out.println("la temperature en Kelvin est de: "+ tempK);
         double Temperature;
-        double Val;
+        int Val;
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Bonjour, saisissez une valeur: ");
         Temperature = sc.nextDouble();
-        System.out.println("Saisissez la conversion que vous souhaiter effectuer : 1) De Celcius vers Kelvin\n"
+        System.out.println("Saisissez la conversion que vous souhaiter effectuer : \n "+" 1) De Celcius vers Kelvin\n"
                 + "2) De Kelvin vers Celcius\n" + "3) De Farenheit vers Celcius\n" + "4) De Celcius vers Farenheit \n" + "5) De Kelvin vers Farenheit\n" + "6) De Farenheit vers Kelvin");
-        Val = sc.nextDouble();
+        Val = sc.nextInt();
+
 
         switch (Val) {
             case 1 -> {
