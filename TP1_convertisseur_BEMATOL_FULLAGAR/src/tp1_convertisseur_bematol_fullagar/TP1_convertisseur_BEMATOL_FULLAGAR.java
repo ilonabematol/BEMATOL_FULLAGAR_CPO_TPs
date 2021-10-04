dec6d18100fc282701536341557073eba783a1fa
ilonabematol <ilonabematol@localhost>
2021-10-04 15:57:56 +0200

    début du tp1 exo 2 suite

--- TP1_convertisseur_BEMATOL_FULLAGAR/src/tp1_convertisseur_bematol_fullagar/TP1_convertisseur_BEMATOL_FULLAGAR.java
+++ TP1_convertisseur_BEMATOL_FULLAGAR/src/tp1_convertisseur_bematol_fullagar/TP1_convertisseur_BEMATOL_FULLAGAR.java
@@ -4,6 +4,8 @@
  */
 package tp1_convertisseur_bematol_fullagar;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ilonabematol
@@ -18,6 +20,23 @@ public class TP1_convertisseur_BEMATOL_FULLAGAR {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+        
+    double tempC=0;
+    double tempK=0;
+    Scanner sc = new Scanner(System.in);
+    System.out.println( "entrer une temperature en celcius:");
+    tempC= sc.nextDouble();
+    tempK = CelciusVersKelvin(tempC);
+    System.out.println("la temperature en Kelvin est de: "+ tempK);
+
     
+    }  
+
+  
+}
+
+    public static double CelciusVersKelvin ( double tempCelcius){
+    double tempKelvin =0;
+    tempKelvin = tempCelcius + 273.15;
+    return tempKelvin;   
 }
