2284e37cef218583362ef0545a40367c112bc18f
ilonabematol <ilonabematol@localhost>
2021-10-04 16:47:10 +0200

    début du tp1 exo 2 suite3

--- TP1_convertisseur_BEMATOL_FULLAGAR/src/tp1_convertisseur_bematol_fullagar/TP1_convertisseur_BEMATOL_FULLAGAR.java
+++ TP1_convertisseur_BEMATOL_FULLAGAR/src/tp1_convertisseur_bematol_fullagar/TP1_convertisseur_BEMATOL_FULLAGAR.java
@@ -21,17 +21,51 @@ public class TP1_convertisseur_BEMATOL_FULLAGAR {
     public static void main(String[] args) {
         // TODO code application logic here
         
-    double tempC=0;
-    double tempK=0;
+    //double tempC=0;
+    //double tempK=0;
+    
+    //Scanner sc = new Scanner(System.in);
+    //System.out.println( "entrer une temperature en celcius:");
+    //tempC= sc.nextDouble();
+    //tempK = CelciusVersKelvin(tempC);
+    //System.out.println("la temperature en Kelvin est de: "+ tempK);
+    
+    double Temperature;
+    double Val;
     
     Scanner sc = new Scanner(System.in);
-    System.out.println( "entrer une temperature en celcius:");
-    tempC= sc.nextDouble();
-    tempK = CelciusVersKelvin(tempC);
-    System.out.println("la temperature en Kelvin est de: "+ tempK);
-
+    System.out.println("Bonjour, saisissez une valeur: ");
+    Temperature = sc.nextDouble();
+    System.out.println("Saisissez la conversion que vous souhaiter effectuer : 1) De Celcius vers Kelvin\n" +
+"2) De Kelvin vers Celcius\n"+ "3) De Farenheit vers Celcius\n"+"4) De Celcius vers Farenheit \n"+ "5) De Kelvin vers Farenheit\n"+ "6) De Farenheit vers Kelvin" );
+    Val = sc.nextDouble();
     
-    }  
+    switch (operator) {
+            case 1 -> {
+                Temperature = CelciusVersKelvin(Temperature);
+                System.out.println(Temperature);
+            }
+            case 2 -> {
+                substract = operande1 - operande2;
+                System.out.println(substract);
+            }
+            case 3 -> {
+                multiply = operande1*operande2;
+                System.out.println(multiply);
+            }
+            case 4 -> {
+                divide =operande1/operande2;
+                System.out.println(divide);
+            }
+            case 5 -> {
+                modulo = operande1%operande2; 
+                System.out.println(modulo);
+            }
+            //default -> {
+                //System.out.println("erreur"); 
+                
+            //}
+
 
 
 
