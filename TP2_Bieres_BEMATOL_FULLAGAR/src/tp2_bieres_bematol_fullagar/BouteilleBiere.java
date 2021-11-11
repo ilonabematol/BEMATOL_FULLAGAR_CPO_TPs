3662c84135822e12e23e7bebea7f58732bc03af6
ilonabematol <ilonabematol@localhost>
2021-11-11 14:36:17 +0100

    tp2 exo1

--- TP2_Bieres_BEMATOL_FULLAGAR/src/tp2_bieres_bematol_fullagar/BouteilleBiere.java
+++ TP2_Bieres_BEMATOL_FULLAGAR/src/tp2_bieres_bematol_fullagar/BouteilleBiere.java
@@ -9,19 +9,53 @@ package tp2_bieres_bematol_fullagar;
  * @author ilonabematol
  */
 public class BouteilleBiere {
-
 String nom;
 Double degreAlcool;
 String brasserie;
 boolean ouverte;
+public  BouteilleBiere(String unNom, double unDegre, String uneBrasserie ){
 
-
+nom = unNom;
+degreAlcool = unDegre;
+brasserie = uneBrasserie;
+ouverte = false;
 
 }
-
-
-Public void lireEtiquette(){
-
   
 
+public void lireEtiquette(){
+       System.out.println("Bouteille de " + nom +" (" + degreAlcool + " degres) \nBrasserie : " + brasserie ) ;
+    
+    
+
 }
+public boolean Decapsuler(){
+    if (ouverte ==false ){
+        ouverte = true;
+        
+    }else{
+       System.out.println(" erreur: biere déjà ouverte");
+       ouverte=false;
+       
+       
+    }
+    return false;
+    
+}
+@Override
+public String toString() {
+   String chaine_a_retourner;
+   chaine_a_retourner = nom + " (" + degreAlcool + " degrés)Ouverte ? ";
+if (ouverte == true ) chaine_a_retourner += "oui" ; else chaine_a_retourner += "non" ;
+return chaine_a_retourner ;
+}
+}
+
+
+
+
+
+
+
+
+
--- TP2_Bieres_BEMATOL_FULLAGAR/src/tp2_bieres_bematol_fullagar/TP2_Bieres_BEMATOL_FULLAGAR.java
+++ TP2_Bieres_BEMATOL_FULLAGAR/src/tp2_bieres_bematol_fullagar/TP2_Bieres_BEMATOL_FULLAGAR.java
@@ -14,14 +14,52 @@ public class TP2_Bieres_BEMATOL_FULLAGAR {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    BouteilleBiere uneBiere= new BouteilleBiere();   
-    uneBiere.nom= "Skoll";
-    uneBiere.degreAlcool = 5.6 ;
-    uneBiere.brasserie= "Trois Brasseurs";
-    uneBiere.ouverte= false ; 
+        
+        // QUESTION 1 a 10
+       //BouteilleBiere Biere= new BouteilleBiere();   
+    //Biere.nom= "Skoll";
+    //Biere.degreAlcool = 5.6 ;
+    ///Biere.brasserie= "Trois Brasseurs";
+    //Biere.ouverte= false ; 
     
-System.out.println("Bouteille de " + uneBiere.nom +" (" + uneBiere.degreAlcool + " degres) \nBrasserie : " + uneBiere.brasserie ) ;
-        // TODO code application logic here
+//System.out.println("Bouteille de " + Biere.nom +" (" + Biere.degreAlcool + " degres) \nBrasserie : " + Biere.brasserie ) ;
+        
+        //BouteilleBiere uneBiere= new BouteilleBiere();
+         //uneBiere.nom = "Cuvée des trolls"; 
+         //uneBiere.degreAlcool = 7.0 ;
+         //uneBiere.brasserie= "Dubuisson";
+         //uneBiere.ouverte= false ; 
+         
+         //uneBiere.lireEtiquette();
+        
+         
+         
+         //BouteilleBiere secondeBiere= new BouteilleBiere();
+         //secondeBiere.nom = "Leffe"; 
+         //secondeBiere.degreAlcool = 6.6 ;
+         ///secondeBiere.brasserie= "Abbaye de Leffe";
+         //secondeBiere.ouverte= false ; 
+         
+         //secondeBiere.lireEtiquette();
+         
+         BouteilleBiere Biere= new BouteilleBiere("Skoll",5.6 , "Trois Brasseurs" );
+         Biere.lireEtiquette(); 
+         BouteilleBiere uneBiere= new BouteilleBiere("Cuvée des trolls", 7.0, "Dubuisson");
+         uneBiere.lireEtiquette();
+         BouteilleBiere autreBiere= new BouteilleBiere("Leffe", 6.6 , "Abbaye de Leffe");
+         autreBiere.lireEtiquette();
+         
+         BouteilleBiere deuxiemeBiere= new BouteilleBiere("Heineken",7.6 , "Pti Comptoir" );
+         //deuxiemeBiere.Decapsuler();
+         deuxiemeBiere.lireEtiquette(); 
+         BouteilleBiere troisiemeuneBiere= new BouteilleBiere("Delirium Red", 7.0, "Café Jules");
+         troisiemeuneBiere.lireEtiquette();
+         //troisiemeuneBiere.Decapsuler();
+         BouteilleBiere derniereBiere= new BouteilleBiere("Brush", 6.6 , "Abbaye de Leffe");
+         //derniereBiere.Decapsuler();
+         derniereBiere.lireEtiquette();         
+         System.out.println(uneBiere) ;
+
     }
     
 }
