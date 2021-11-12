5123692f6e51dc345de0fa041dcbabb0ff1221a9
ilonabematol <ilonabematol@localhost>
2021-11-12 16:43:59 +0100

    Console création des différentes classes

---
+++ SP4_console_BEMATOL_FULLAGAR/src/sp4_console_bematol_fullagar/Joueur.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_bematol_fullagar;
+
+/**
+ *
+ * @author ilonabematol
+ */
+public class Joueur {
+    String nom;
+    String couleur;
+    int ListeJetons []= new int[21] ;
+    int nombreJetonsRestants= 0;
+    int nombreDesintegrateurs=0;
+   
+    public void Joueur(String nomJoueur) {
+    
+}
+    
+    public void affecterCouleur(String couleurJoueur){
+        
+    }
+    public void ajouterJeton(Jeton ajoutJeton){
+        
+    }
+    public void obtenirDesintegrateur(){
+        
+    }
+    public void utiliserDesintegrateur(){
+        
+    }
+}
